90d584465bb9311b50d4238ef6c127fa85b9fb3e
KellerMarika <marikakeller92@gmail.com>
2023-02-13 17:43:13 +0100

    ma come sono prolissa e dispersiva

--- resources/views/profile/partials/dashboard/main.blade.php
+++ resources/views/profile/partials/dashboard/main.blade.php
@@ -1,7 +1,8 @@
 <main class=" w-100 ps-5 p-4 overflow-x-hidden">
 
 {{-- if active_Section === "profile"  --}}
-    @include('profile.partials.dashboard.main.profile')
+   {{--  @include('profile.partials.dashboard.main.profile') --}}
+    @include('profile.partials.dashboard.main.projects')
 
 
 </main>
